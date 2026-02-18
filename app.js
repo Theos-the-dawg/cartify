0cbd0e1717e9b1db4f7fa3ad13c3e47c8ca986a0
Theos-the-dawg <motheo13@outlook.com>
2026-02-18 13:00:22 +0200

    master-addition

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
-const path =require('path');
+const express = require('express'); // this is where we import the package for express
+const app = express(); // initialize the express app 
+const path = require('path'); // sets the file path for the files(html templates that will be rendered)
 
-const host = '127.0.0.1';
-const port = 3000;
+const host = '127.0.0.1'; // host often the IP address but for now localhost
+const port = 3000;//port number address for specific functions 
 
+// in express a view engine is the tool used for telling express how this page 
 app.set('view engine', 'ejs'); 
 
 app.set('views', path.join(__dirname, './public/views/')); 
@@ -15,6 +16,12 @@ app.get('/',(req,res) =>{
   res.render('index', { title: 'My Express App', message: 'Hello, World!' });
 })
 
+app.get('/home',(req,res) =>{
+  res.render('index', { title: 'home', message: 'Home' });
+})
+
 app.listen(port,()=>{
     console.log(`running on ${host}:${port}`)
 });
+
+
