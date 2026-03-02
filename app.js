137635c04d87a180ba6cbbe7c73ad1a1e9498a68
Theos-the-dawg <motheo13@outlook.com>
2026-03-02 13:46:37 +0200

    pre-prod-version

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express'); // this is where we import the package for express
-const app = express(); // initialize the express app 
+const app = express(); // initialize the express router 
+// const router = express.Router();
 const path = require('path'); // sets the file path for the files(html templates that will be rendered)
+
 
 const host = '127.0.0.1'; // host often the IP address but for now localhost
 const port = 3000;//port number address for specific functions 
@@ -13,7 +15,7 @@ app.set('views', path.join(__dirname, './public/views/'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res) =>{
-  res.render('index', { title: 'My Express App', message: 'Hello, World!' });
+  res.render('index', { title: 'My Express router', message: 'Hello, World!' });
 })
 
 app.get('/home',(req,res) =>{
@@ -21,7 +23,7 @@ app.get('/home',(req,res) =>{
 })
 
 app.listen(port,()=>{
-    console.log(`running on ${host}:${port}`)
+    console.log(`running on http://${host}:${port}`)
 });
 
 
---
+++ index.js
@@ -0,0 +1,7 @@
+let user = {
+
+    FirstName: "",
+    LastName: "",
+    UserName:"",
+    Password:"*********"
+};
